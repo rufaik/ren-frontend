797ecfa94bec782a586864bcc634a85804793d17
Kems <rufai@hotmail.co.uk>
2022-03-24 12:05:01 +0000

    image6

--- src/pages/NewListing.js
+++ src/pages/NewListing.js
@@ -101,7 +101,7 @@ const handleSubmit = async (event) => {
     event.preventDefault()
     const coins = Math.round(rentalValue * 5)
     console.log('handling1', image)
-    console.log('handling2', image1)
+    console.log('handling2', images1)
 
     if(!user){
       setError('Please log in first')
