6040443bce08c7d3f9b13b782db8dbb339d0c985
Kems <rufai@hotmail.co.uk>
2022-12-30 14:02:31 +0000

    add $ env pk1

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,7 +5,6 @@ import CheckoutForm from './CheckoutForm';
 
 const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PK}`);
 
-console.log("stripePromise", stripePromise)
 
 
 export default () =>{
