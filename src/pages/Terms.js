9d1e50e7de8d1efea94b116231d83560c73d2a8a
Kems <rufai@hotmail.co.uk>
2023-03-26 23:43:15 +0100

    markdown 26th march

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -5,6 +5,8 @@ import {fromProductSlugToUrl} from '../utils/products'
 import {API_URL} from '../utils/urls'
 import {UserContext} from '../context/UserContext'
 import Footer from '../components/Footer'
+import ReactMarkdown from 'react-markdown'
+
 
 
 
@@ -51,20 +53,23 @@ const getContent = async (user) => {
     {content &&
       <div>
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div className="text-center">
+          <div className="">
             <h2 
-              className={create === 'darkbg' ? "text-base font-semibold text-white tracking-wide uppercase" : "text-base font-semibold text-indigo-600 tracking-wide uppercase"}
+              className={create === 'darkbg' ? "text-center text-base font-semibold text-white tracking-wide uppercase" : "text-center text-base font-semibold text-indigo-600 tracking-wide uppercase"}
             >{content[0].smallTitle}</h2>
             <p 
-            className={create === 'darkbg' ? "mt-6 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl" : "mt-6 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"}
+            className={create === 'darkbg' ? "text-center mt-6 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl" : "text-center mt-6 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"}
             >
               {content[0].largeTitle}
             </p>
-            <p 
-            className={create === 'darkbg' ? "max-w-xl mt-8 mx-auto text-xl text-white" : "max-w-xl mt-8 mx-auto text-xl text-gray-500"}
+            <div 
+            className={create === 'darkbg' ? "text-center max-w-xl mt-8 mx-auto text-xl text-white" : "text-center max-w-xl mt-8 mx-auto text-xl text-gray-500"}
             >
+              
+               <ReactMarkdown>
               {content[0].mainBody}
-            </p>
+              </ReactMarkdown>
+            </div>
           </div>
         </div>
         <Footer />
